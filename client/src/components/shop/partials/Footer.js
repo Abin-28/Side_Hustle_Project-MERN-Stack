d7ffa5ceb2e436483a2130592e8dadf21bba0b7c
Creator Hub <abinskaria28@gmail.com>
2025-09-03 19:48:37 +0530

    chore: Update footer and navbar components to redirect Contact Us To Footer.

--- client/src/components/shop/partials/Footer.js
+++ client/src/components/shop/partials/Footer.js
@@ -5,10 +5,17 @@ const Footer = (props) => {
   return (
     <Fragment>
       <footer
+        id="contact"
         style={{ background: "#303031", color: "#87898A" }}
-        className="z-10 py-6 px-4 md:px-12 text-center"
+        className="z-10 py-6 px-4 md:px-12 text-center space-y-2"
       >
-        Develop & Design Hasan-py © Copyright {moment().format("YYYY")}
+        <div>
+          Develop & Design AbinSkaria © Copyright 2025
+        </div>
+        <div className="text-sm">
+          Contact us: <a href="tel:+1234567890" className="underline">+1234567890</a> ·
+          <a href="mailto:xyz@auroramart.in" className="underline ml-1">xyz@auroramart.in</a>
+        </div>
       </footer>
     </Fragment>
   );
